257491b0ecc06af9ba1e73676213a93f924a561b
sergioruydev <sergioruyenator@gmail.com>
2023-11-07 15:42:05 -0300

    service layer in progress

--- src/test/java/belly/infra/UserServiceWithUserMemoryRepositoryTest.java
+++ src/test/java/belly/infra/UserServiceWithUserMemoryRepositoryTest.java
@@ -1,8 +1,32 @@
 package belly.infra;
 
+import belly.domain.User;
+import belly.exceptions.ValidationException;
 import belly.service.UserService;
+import org.junit.jupiter.api.Test;
+
+import static belly.builders.UserBuilder.oneUser;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceWithUserMemoryRepositoryTest {
 
-    private UserService service = new UserService(new UserMemoryRepository());
+    private static UserService service = new UserService(new UserMemoryRepository());
+
+    @Test
+    public void shouldSaveValidUser() {
+
+       User user = service.save(oneUser().withId(null).createEntity());
+
+        assertNotNull(user.getId());
+    }
+
+    @Test
+    public void shouldRejectExistedUser() {
+
+        ValidationException exception = assertThrows(ValidationException.class, () ->
+                service.save(oneUser().withId(null).createEntity())
+        );
+        assertEquals("asasasa", exception.getMessage());
+    }
+
 }
