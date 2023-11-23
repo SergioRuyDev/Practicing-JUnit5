dc3ea4e9ec524b00679c515a55c147e84fc6e242
sergioruydev <sergioruyenator@gmail.com>
2023-11-23 15:59:12 -0300

    Using mockito annotations.

--- src/test/java/belly/service/UserServiceTest.java
+++ src/test/java/belly/service/UserServiceTest.java
@@ -1,26 +1,41 @@
 package belly.service;
 
-import belly.builders.UserBuilder;
 import belly.domain.User;
 import belly.service.repositories.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Optional;
 
+import static belly.builders.UserBuilder.oneUser;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 public class UserServiceTest {
 
+    @Mock
+    private UserRepository repository;
+
+    @InjectMocks
     private UserService service;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    //    @AfterEach
+//    void tearDown() {
+//        verifyNoInteractions(repository);
+//    }
 
     @Test
     public void shouldReturnEmptyWhenUserNotExist() {
-
-        UserRepository repository = Mockito.mock(UserRepository.class);
-
-        service = new UserService(repository);
 
 //        Mockito.when(repository.getUserByEmail("mail@email.com")).thenReturn(Optional.empty());
 
@@ -31,16 +46,33 @@ public class UserServiceTest {
     @Test
     public void shouldReturnUserByEmail() {
 
-        UserRepository repository = Mockito.mock(UserRepository.class);
-
-        service = new UserService(repository);
-
         when(repository.getUserByEmail("mail@email.com"))
-                .thenReturn(Optional.of(UserBuilder.oneUser().createEntity()));
+                .thenReturn(Optional.of(oneUser().createEntity()));
 
         Optional<User> user = service.getUserByEmail("mail@email.com");
+
         assertTrue(user.isPresent());
-        verify(repository).getUserByEmail("mail@email.com");
-        verify(repository, times(1)).getUserByEmail("mail@email.com");
+
+//        verify(repository, atLeastOnce()).getUserByEmail("mail@email.com");
+//        verify(repository,never()).getUserByEmail("other@email.com");
+//        verifyNoInteractions(repository);
+    }
+
+    @Test
+    public void shouldSaveUser() {
+
+        User userToSave = oneUser().withId(null).createEntity();
+
+//        when(repository.getUserByEmail(userToSave.getEmail()))
+//                .thenReturn(Optional.empty());
+
+        when(repository.save(userToSave))
+                .thenReturn(oneUser().createEntity());
+
+        User savedUser = service.save(userToSave);
+        assertNotNull(savedUser.getId());
+
+        verify(repository).getUserByEmail(userToSave.getEmail());
+        verify(repository).save(userToSave);
     }
 }
